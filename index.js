94f7ca8af991b75f457102ab582402287be90a9b
unknown <dasilvadeoliveirafabio@gmail.com>
2022-05-27 19:05:51 -0300

    teste de deploy 4

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 const app = express()
 // const conexao = require('./conexao')
-const rotas = require('./Rotas')
-const cors = require('cors');  
-app.use(cors());
+// const rotas = require('./Rotas')
+// const cors = require('cors');  
+// app.use(cors());
 
 //app.use(rotas)
 app.get("/",(req,res)=>{
